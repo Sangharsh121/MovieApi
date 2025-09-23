6516b87709924f32d95b8c9e88ced5813a890630
Sangharsh121 <srghadse@gmail.com>
2025-09-23 21:21:49 +0530

    Initial commit

--- src/main/java/dev/sanga/moviesapi/MovieController.java
+++ src/main/java/dev/sanga/moviesapi/MovieController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -19,5 +21,10 @@ public class MovieController {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(),HttpStatus.OK);
         
     }
+    @GetMapping("/{imdbid}")
+        public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbid){
+            return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbid),HttpStatus.OK);
+        }
+    
     
 }
