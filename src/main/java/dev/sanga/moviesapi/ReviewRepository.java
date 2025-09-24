539bb7a4552414b2ad150c40ccd9249df9e1b44a
Sangharsh121 <srghadse@gmail.com>
2025-09-24 10:53:46 +0530

    Initial commit

---
+++ src/main/java/dev/sanga/moviesapi/ReviewRepository.java
@@ -0,0 +1,10 @@
+package dev.sanga.moviesapi;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+    
+}
