5657737c08cbfe5a9ef846ac718080cc15d899a7
Sangharsh121 <srghadse@gmail.com>
2025-10-02 12:47:57 +0530

    Initial commit

--- src/main/java/dev/sanga/moviesapi/MovieController.java
+++ src/main/java/dev/sanga/moviesapi/MovieController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -13,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
+@CrossOrigin
 public class MovieController {
     @Autowired
     private MovieService movieService;
--- src/main/java/dev/sanga/moviesapi/ReviewController.java
+++ src/main/java/dev/sanga/moviesapi/ReviewController.java
@@ -7,11 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/reviews")
+@CrossOrigin
 public class ReviewController {
     @Autowired
     private ReviewService reviewService;
